cdd779b4980ef23f8338770de9b0a658d577f55c
peteray-dev <peteraydev@gmail.com>
2025-02-05 21:51:09 +0000

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Ayomide Popoola',
+    author_email= 'peteraydev@gmail.com',
+    packages= find_packages(), #find __init__ adn use as local package
+    install_requires = []
+
+)
